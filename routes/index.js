fef87bb948467474871d8e35d276914ba48df52a
Jared Radin <jared.radin@gmail.com>
2015-01-17 18:56:53 -0800

    add portfolio route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio1', {title : 'Portfolio #1'});
+})
+
 module.exports = router;
