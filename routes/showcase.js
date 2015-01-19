60c54e61e87f5e75ee23b06ea72f4a5a8b5c05de
Jared Radin <jared.radin@gmail.com>
2015-01-18 16:00:56 -0800

    portfolio shows one image, has styled arrows

--- routes/showcase.js
+++ routes/showcase.js
@@ -3,28 +3,28 @@ var router = express.Router();
 
 var imgArr = [
 {
-  "src": "./images/test1.jpg",
-  "alt": "First Test Image"
+  src: "./images/test1.jpg",
+  alt: "First Test Image"
 },
 {
-  "src": "./images/test2.jpg",
-  "alt": "Second Test Image"
+  src: "./images/test2.jpg",
+  alt: "Second Test Image"
 },
 {
-  "src": "./images/test3.jpg",
-  "alt": "Third Test Image"
+  src: "./images/test3.jpg",
+  alt: "Third Test Image"
 },
 {
-  "src": "./images/test4.jpg",
-  "alt": "Fourth Test Image"
+  src: "./images/test4.jpg",
+  alt: "Fourth Test Image"
 },
 {
-  "src": "./images/test5.jpg",
-  "alt": "Fifth Test Image"
+  src: "./images/test5.jpg",
+  alt: "Fifth Test Image"
 },
 {
-  "src": "./images/test6.jpg",
-  "alt": "Sixth Test Image"
+  src: "./images/test6.jpg",
+  alt: "Sixth Test Image"
 }
 ]
 
@@ -39,7 +39,7 @@ router.get('/', function(req, res, next) {
 })
 
 router.get('/portfolio', function(req, res, next) {
-  res.render('portfolio', {'images': imgArr});
+  res.render('portfolio', imgArr[0]);
 })
 
 module.exports = router;
