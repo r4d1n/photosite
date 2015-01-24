59059bf1d09e80a25ef66c4ccf632c0a3cb4ced7
Jared Radin <jared.radin@gmail.com>
2015-01-24 12:05:53 -0800

    fix issue with backwards cycling through portfolio causing negative i and prev

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -13,28 +13,17 @@ router.get('/', function(req, res) {
     prev: 0 + "/" + prev,
     next: 0 + "/" + next
   })
+  console.log(res);
 })
 
 router.get('/:set/:id', function(req, res) {
   var b = req.params.set;
   var i = req.params.id ? req.params.id : 0;
+  if (i <= 0) {i = 0};
   // adjust var prev for 0th index
   var prev = i === 0 ? imgList[b].length - 1 : parseInt(i) - 1;
   // adjust var next for last in array
   var next = i >= imgList[b].length - 1 ? 0 : parseInt(i) + 1;
-
-  // if (i <= 0) {
-  //   prev = imgList[b].length - 1;
-  // } else {
-  //   prev = parseInt(i) - 1;
-  // };
-  //
-  // if (i >= imgList[b].length - 1) {
-  //   next = 0;
-  // } else {
-  //   next = parseInt(i) + 1;
-  // };
-
   console.log("b: " + b, "i: " + i, "prev: " + prev, "next: " + next);
   res.send({
     img: imgList[b][i],
