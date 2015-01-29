34839edeb546093d4d695c09d55b7f4135abd0ef
Jared Radin <jared.radin@gmail.com>
2015-01-28 17:01:37 -0800

    remove transition animation

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -15,12 +15,12 @@ $(document).ready(function() {
     var $img = $('#portfolio-img');
     var href = $(this).attr('href');
     $.get(href, function(res) {
-      $img.fadeOut('fast', function() {
+      // $img.fadeOut('slow', function() {
         $img.attr('src', res.img.src).attr('alt', res.img.alt);
-        $img.fadeIn('slow');
         $('.next').attr('href', '/portfolio/' + res.next);
         $('.prev').attr('href', '/portfolio/' + res.prev);
-      });
+        // $img.fadeIn('fast');
+      // });
     });
     return false;
   });
